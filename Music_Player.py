465d06ed790924def745f582fdaf186268ec3072
AHMET YASİN TAZEGÜL <97638223+tazegul20@users.noreply.github.com>
2023-09-19 16:14:29 +0300

    Delete Music_Player.py

--- Music_Player.py
+++
@@ -1,8 +0,0 @@
-import pygame
-import time
-pygame.mixer.init()
-pygame.mixer.music.load("C:/Users/ahm_2/Downloads/Ceg - First Class (Official Video).mp3")
-pygame.mixer.music.play()
-
-while pygame.mixer.music.get_busy() == True:
-    continue
